1a750418934f9abd9d8fb088999b8dde813183eb
Saurabh Bhardwaj <aryan.saurabhbhardwaj@gmail.com>
2024-02-18 21:36:26 +0000

    terraform added

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,18 @@
+# Resource Group/Location
+variable "application_type" {}
+variable "resource_type" {}
+variable "location" {}
+variable "resource_group" {}
+variable "subnet_id" {}
+variable "public_ip_address_id" {}
 
+variable "admin_username" {
+   description = "User name - VMs"
+   default     = "aryan"
+}
+
+variable "admin_password" {
+   description  = "Default password"
+   default      = "password@123"
+}
+variable "rsa_keygen" {}
