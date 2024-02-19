597037e96c3632ef0d4f377d140638e02b09410e
Saurabh BHardwaj <aryan.saurabhbhardwaj@gmail.com>
2024-02-19 12:48:56 +0000

    new details about access

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,6 +1,8 @@
 
 variable "public_ip" {}
 variable "resource_group" {}
+variable "application_type" {}
+variable "resource_type" {}
 variable "location" {}
 variable "name" {}
 variable "subnet_id" {}
