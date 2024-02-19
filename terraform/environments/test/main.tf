601341b3c20a82d9f72287526f32ce402690b238
Saurabh BHardwaj <aryan.saurabhbhardwaj@gmail.com>
2024-02-19 12:39:33 +0000

    new details about access

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -22,7 +22,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${var.resource_group_name}"
+  resource_group       = "${var.resource_group}"
   address_prefix_test  = "${var.address_prefix_test}"
 }
 
@@ -31,7 +31,7 @@ module "nsg-test" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
-  resource_group   = "${var.resource_group_name}"
+  resource_group   = "${var.resource_group}"
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
 }
@@ -40,21 +40,21 @@ module "appservice" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
-  resource_group   = "${var.resource_group_name}"
+  resource_group   = "${var.resource_group}"
 }
 module "publicip" {
   source           = "../../modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group   = "${var.resource_group_name}"
+  resource_group   = "${var.resource_group}"
 }
 module "vm" {
   source          = "../../modules/vm"
   name            = "ducda-VM"
   location        = "${var.location}"
   subnet_id       = module.network.subnet_id_test
-  resource_group  = "${var.resource_group_name}"
+  resource_group  = "${var.resource_group}"
   public_ip       = module.publicip.public_ip_address_id
   admin_username  = "${var.admin_username}"
   admin_password  = "${var.admin_password}"
