fefd6e75be3fd53332e06af4592d65f3d46f333c
Saurabh BHardwaj <aryan.saurabhbhardwaj@gmail.com>
2024-02-19 10:50:24 +0000

    new details about access

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -14,6 +14,7 @@ terraform {
   }
 }
 
+
 module "network" {
   source               = "../../modules/network"
   address_space        = "${var.address_space}"
@@ -21,7 +22,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${module.resource_group.resource_group_name}"
+  resource_group       = "${var.resource_group_name}"
   address_prefix_test  = "${var.address_prefix_test}"
 }
 
@@ -30,7 +31,7 @@ module "nsg-test" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group_name}"
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
 }
@@ -39,22 +40,22 @@ module "appservice" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group_name}"
 }
 module "publicip" {
   source           = "../../modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group_name}"
 }
 module "vm" {
-source           = "../../modules/vm"
-location         = "${var.location}"
-application_type = "${var.application_type}"
-resource_type    = "vm"
-resource_group   = "${module.resource_group.resource_group_name}"
-subnet_id         = "${module.network.subnet_id_test}"
-public_ip_address_id = "${module.publicip.public_ip_address_id}"
-rsa_keygen           = var.rsa_keygen
+  source          = "../../modules/vm"
+  location        = "${var.location}"
+  subnet_id       = module.network.subnet_id_test
+  resource_group  = "${var.resource_group_name}"
+  public_ip       = module.publicip.public_ip_address_id
+  admin_username  = "${var.admin_username}"
+  admin_password  = "${var.admin_password}"
+  rsa_keygen           = var.rsa_keygen
 }
